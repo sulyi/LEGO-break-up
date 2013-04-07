bc9c4595850e19968b86a7a72d711d5895252ac0
sulyi <sulyi.gbox@gmail.com>
2013-04-07 16:26:27 +0200

    Re-arrange

--- main.py
+++ main.py
@@ -13,16 +13,27 @@ from OpenGL.GL import *
 from OpenGL.GLU import *   
 
 import lego
-from wicd.translations import language
-from distutils.command.config import LANG_EXT
 
-
+class button(pygame.Rect):
+    def __init__( self, name, x, y, width, height, color, focus_color ):
+        self.name = name
+        super(button, self ).__init__( x, y, width, height )
+        self.color = color
+        self.focus_color = focus_color
+        
+    def draw( self, focused = False ):
+        glColor3fv( self.color if not focused else self.focus_color)
+        glBindTexture(GL_TEXTURE_2D, 0)
+        glRectiv( self.topleft, self.bottomright )
+        
+    def is_hit_by(self, point):
+        return self.contains(pygame.Rect( point, (0,0) ))
 
 if __name__ == '__main__':
-    
-    print "Initializing translator ...",
+
+    print "Initializing locale ...",
     gettext.install('default', os.path.join('.','locale'))
-    po = {"width":  _("width"), "depth" :  _("length"), "height" : _("height") }
+    po = {"width":  _("Width"), "depth" :  _("Length"), "height" : _("Height") }
     for k in po:
         po[k] = unicode(po[k],'utf8')
     print "Done"
@@ -31,7 +42,7 @@ if __name__ == '__main__':
     pygame.init()
     print "Done"
     
-    print "Creating screen ...",
+    print "Initializing screen ...",
     screen = pygame.display.set_mode ((800,600), pygame.OPENGL|pygame.DOUBLEBUF, 24)
     print "Done"
     
@@ -51,8 +62,10 @@ if __name__ == '__main__':
     
     # draw dialog screen 
     
-    button = pygame.Rect(10,10, 50, 50)
-    
+    buttons = { button( "widthbutton", 60,115, 100,100, (0.8, 0.8, 0.0), (0.3, 0.8, 0.5) ),
+                button( "depthbutton", 60,330, 100,100, (0.8, 0.8, 0.0), (0.3, 0.8, 0.5) )
+               }
+                  
     print "Loading textures ...",
     
     textures = dict()
@@ -63,26 +76,34 @@ if __name__ == '__main__':
     width = image.get_width()
     height = image.get_height()
     
-    textures["ui"] =  ( lego.load_2d_texture(imageData, width , height), (1.0, 1.0, 1.0), 0, 0, width, height )
+    textures["ui"] =  ( lego.load_2d_texture(imageData, width , height),
+                        (1.0, 1.0, 1.0), 0, 0, width, height )
     
-    font = pygame.font.SysFont("courier", 32)
+    font = pygame.font.SysFont("courier", 32, True, True)
     
-    text = font.render( po["width"], True, (255, 255, 255) )
-    textures["widthtext"] = ( lego.load_2d_texture( pygame.image.tostring(text, 'RGBA', True), 
-                                                 text.get_width(), text.get_height() ),   
-                           (0.6, 0.3, 0.7), 20, 20, 190, 90 )
+    text_shadow = font.render( po["width"], True, (192, 64, 128) )
+    text = font.render( po["width"], True, (153, 76, 178) )
+    text_shadow.blit(text, (1,1))
     
-    text = font.render( po["depth"], True, (255, 255, 255) )
+    textures["widthtext"] = ( lego.load_2d_texture( pygame.image.tostring(text_shadow, 'RGBA', True), 
+                                                 text_shadow.get_width(), text_shadow.get_height() ),   
+                           (1.0, 1.0, 1.0), 20, 20, 190, 90 )
     
-    textures["depthtext"] = ( lego.load_2d_texture( pygame.image.tostring(text, 'RGBA', True), 
-                                                 text.get_width(), text.get_height() ),   
-                           (0.6, 0.3, 0.7), 20, 235, 190, 305 )
+    text_shadow = font.render( po["depth"], True, (192, 64, 128) )
+    text = font.render( po["depth"], True, (153, 76, 178) )
+    text_shadow.blit(text, (1,1))
     
-    text = font.render( po["height"], True, (255, 255, 255) )
+    textures["depthtext"] = ( lego.load_2d_texture( pygame.image.tostring(text_shadow, 'RGBA', True), 
+                                                 text_shadow.get_width(), text_shadow.get_height() ),   
+                           (1.0, 1.0, 1.0), 20, 235, 190, 305 )
     
-    textures["heighttext"] = ( lego.load_2d_texture( pygame.image.tostring(text, 'RGBA', True), 
-                                                 text.get_width(), text.get_height() ),   
-                           (0.6, 0.3, 0.7), 20, 20, 190, 90 )  
+    text_shadow = font.render( po["height"], True, (192, 64, 128) )
+    text = font.render( po["height"], True, (153, 76, 178) )
+    text_shadow.blit(text, (1,1))
+    
+    textures["heighttext"] = ( lego.load_2d_texture( pygame.image.tostring(text_shadow, 'RGBA', True), 
+                                                 text_shadow.get_width(), text_shadow.get_height() ),   
+                           (1.0, 1.0, 1.0), 20, 20, 190, 90 )  
     
     print "Done"
     
@@ -95,14 +116,14 @@ if __name__ == '__main__':
     rot_speed = 1.5
     ticker = pygame.time.Clock()
     running = True
+    focused = None
+    hit = None
     while running:
-        
         for event in pygame.event.get():
             if event.type == pygame.QUIT or event.type == pygame.KEYUP:
                 running = False
             if event.type == pygame.MOUSEBUTTONUP:
-                print event.pos
-                print button.contains( pygame.Rect(event.pos,(0,0)) ) 
+                hit = event.pos
         
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
         
@@ -123,14 +144,17 @@ if __name__ == '__main__':
         lego.draw_ortho_layer( *textures["ui"] )
         
         lego.draw_ortho_layer( *textures["widthtext"])
-        glColor3f(0.8, 0.8, 0.0)
-        glBindTexture(GL_TEXTURE_2D, 0)
-        glRecti(60,115,160,215)
-        
         lego.draw_ortho_layer( *textures["depthtext"])
-        glColor3f(0.8, 0.8, 0.0)
-        glBindTexture(GL_TEXTURE_2D, 0)
-        glRecti(60,330,160,430)
+        
+        for b in  buttons:
+            b.draw( b is focused )
+            if hit and b.is_hit_by(hit):
+                focused = b
+                hit = None
+        if hit is not None:
+            hit = None
+            focused = None
+            
         
         glPopMatrix()
     
